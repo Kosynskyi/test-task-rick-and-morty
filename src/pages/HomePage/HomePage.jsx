34abeecf21cb05b5d71c0724e6ae269499a2c6a4
Kosynskyi <kosynskyi.vova@gmail.com>
2023-03-10 20:14:17 +0200

    save home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Filter from 'components/Filter';
 import CharacterList from 'components/CharacterList';
+import Logo from 'components/Logo';
+import { Box } from 'utils/Box';
 
 const HomePage = () => {
   return (
-    <>
+    <Box pb="188px" pt="86px" width="100%">
+      <Logo />
       <Filter />
       <CharacterList />
-    </>
+    </Box>
   );
 };
 
