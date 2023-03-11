ba9fcc845977e9fc5614a18fdf8e81ea314656a0
Kosynskyi <kosynskyi.vova@gmail.com>
2023-03-11 11:17:56 +0200

    add go back

--- src/components/CharacterDetails/CharacterDetails.js
+++ src/components/CharacterDetails/CharacterDetails.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import { Box } from 'utils/Box';
 import { getCharacterInfo } from 'services/API/API';
 import { placeholderMissingInfo } from 'helpers/placeholderMissingInfo';
+import { ReactComponent as GoBackIcon } from 'assets/arrow_back.svg';
 import {
+  GoBackLink,
+  GoBackText,
+  // StyledGoBackIcon,
   CharacterPhoto,
   CharacterName,
   CharacterInfoTitle,
@@ -15,6 +19,8 @@ import {
 const CharacterDetails = () => {
   const [character, setCharacter] = useState(null);
   const { id } = useParams();
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? '/';
   console.log(id);
 
   useEffect(() => {
@@ -29,31 +35,38 @@ const CharacterDetails = () => {
   const { image, name, gender, status, species, origin, type } = character;
 
   return (
-    <Box pt="31px" width="413px">
-      <CharacterPhoto src={image} alt={name} width="300px" />
-      <CharacterName>{placeholderMissingInfo(name)}</CharacterName>
-      <CharacterInfoTitle>Informations</CharacterInfoTitle>
-      <InfoElementWrapper>
-        <DescriptionTitle>Gender</DescriptionTitle>
-        <CharacterInfo>{placeholderMissingInfo(gender)}</CharacterInfo>
-      </InfoElementWrapper>
-      <InfoElementWrapper>
-        <DescriptionTitle>Status</DescriptionTitle>
-        <CharacterInfo>{placeholderMissingInfo(status)}</CharacterInfo>
-      </InfoElementWrapper>
-      <InfoElementWrapper>
-        <DescriptionTitle>Specie</DescriptionTitle>
-        <CharacterInfo>{placeholderMissingInfo(species)}</CharacterInfo>
-      </InfoElementWrapper>
-      <InfoElementWrapper>
-        <DescriptionTitle>Origin</DescriptionTitle>
-        <CharacterInfo>{placeholderMissingInfo(origin.name)}</CharacterInfo>
-      </InfoElementWrapper>
-      <InfoElementWrapper>
-        <DescriptionTitle>Type</DescriptionTitle>
-        <CharacterInfo>{placeholderMissingInfo(type)}</CharacterInfo>
-      </InfoElementWrapper>
-    </Box>
+    <>
+      <Box display="flex" justifyContent="flex-start" alignItems="center">
+        <GoBackLink to={backLinkHref}>
+          <GoBackIcon /> <GoBackText>go back</GoBackText>
+        </GoBackLink>
+      </Box>
+      <Box pt="31px" width="413px">
+        <CharacterPhoto src={image} alt={name} width="300px" />
+        <CharacterName>{placeholderMissingInfo(name)}</CharacterName>
+        <CharacterInfoTitle>Informations</CharacterInfoTitle>
+        <InfoElementWrapper>
+          <DescriptionTitle>Gender</DescriptionTitle>
+          <CharacterInfo>{placeholderMissingInfo(gender)}</CharacterInfo>
+        </InfoElementWrapper>
+        <InfoElementWrapper>
+          <DescriptionTitle>Status</DescriptionTitle>
+          <CharacterInfo>{placeholderMissingInfo(status)}</CharacterInfo>
+        </InfoElementWrapper>
+        <InfoElementWrapper>
+          <DescriptionTitle>Specie</DescriptionTitle>
+          <CharacterInfo>{placeholderMissingInfo(species)}</CharacterInfo>
+        </InfoElementWrapper>
+        <InfoElementWrapper>
+          <DescriptionTitle>Origin</DescriptionTitle>
+          <CharacterInfo>{placeholderMissingInfo(origin.name)}</CharacterInfo>
+        </InfoElementWrapper>
+        <InfoElementWrapper>
+          <DescriptionTitle>Type</DescriptionTitle>
+          <CharacterInfo>{placeholderMissingInfo(type)}</CharacterInfo>
+        </InfoElementWrapper>
+      </Box>
+    </>
   );
 };
 
--- src/components/CharacterDetails/CharacterDetails.styled.js
+++ src/components/CharacterDetails/CharacterDetails.styled.js
@@ -1,4 +1,20 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const GoBackLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+  text-decoration: none;
+`;
+
+export const GoBackText = styled.span`
+  margin-left: ${props => props.theme.space[3]}px;
+  font-size: ${props => props.theme.fontSizes.l};
+  font-weight: ${props => props.theme.fontWeights.bold};
+  line-height: ${props => props.theme.lineHeights.secondary};
+  color: ${props => props.theme.colors.black};
+`;
 
 export const CharacterPhoto = styled.img`
   border-radius: ${props => props.theme.radii.round};
